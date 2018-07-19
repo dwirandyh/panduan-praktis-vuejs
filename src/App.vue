b866f146d55a15ea6b19b84d44c9b80ec68c29bd
Unknown <dwirandyherdinanto@gmail.com>
2018-07-20 02:35:24 +0700

    BAB 3 Form

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">BAB 1</router-link> |
-      <router-link to="/bab2">BAB 2</router-link>
+      <router-link to="/bab2">BAB 2</router-link> |
       <router-link to="/bab3">BAB 3</router-link>
     </div>
     <router-view/>
--- src/views/BabTiga.vue
+++ src/views/BabTiga.vue
@@ -1,78 +1,58 @@
 <template>
     <div class="about">
-        <h1>BAB 3 Computed, Methods & Watcher</h1>
-        <h3>Computed</h3>
-        <p>Hasil dari {{ bilangan1 }} + {{ bilangan2 }} = {{ jumlah }}</p>
-        <h3>Method</h3>
-        <p>Hasil dari {{ bilangan1 }} + {{ bilangan2 }} = {{ total }}</p>
-        <button v-on:click="proses">Hitung</button>
-        <h3>Watcher</h3>
-        <p>Keterangan : {{ keterangan }}</p>
-        <p><input type="text" v-model="isi"> {{ keteranganAksi }}</p>
-        <h3>Watcher Form Login</h3>
-        <input type="text" v-model="password"/>
-        <input type="text" v-model="passwordRepeat"/>
-        {{ keteranganLogin }}
+        <h1>Playing With Form</h1>
+        <table>
+            <tr>
+                <td>Textbox</td>
+                <td><input type="text" v-model="pesanTextBox"></td>
+                <td>{{ pesanTextBox }}</td>
+            </tr>
+            <tr>
+                <td>Textarea</td>
+                <td><textarea v-model="pesanTextArea"></textarea></td>
+                <td><span style="white-space: pre-line">{{ pesanTextArea}}</span></td>
+            </tr>
+            <tr>
+                <td>Radio Button</td>
+                <td><input type="radio" v-model="pilihanRadio" value="Laki - Laki">Laki - Laki <input type="radio"
+                                                                                                      v-model="pilihanRadio"
+                                                                                                      value="Perempuan">Perempuan
+                </td>
+                <td>{{ pilihanRadio }}</td>
+            </tr>
+            <tr>
+                <td>Checkbox</td>
+                <td><input type="checkbox" v-model="hobi" value="Code">Code <input type="checkbox" v-model="hobi"
+                                                                                   value="Eat">Eat <input
+                        type="checkbox" v-model="hobi" value="Repeat">Repeat
+                </td>
+                <td>{{ hobi }}
+                    <ul>
+                        <li v-for="row in hobi">{{ row }}</li>
+                    </ul>
+                </td>
+            </tr>
+            <tr>
+                <td>Dropdown</td>
+                <td><select v-model="dropdownKelamin">
+                    <option value="Laki - Laki">Laki - Laki</option>
+                    <option value="Perempuan">Perempuan</option>
+                </select></td>
+                <td>{{ dropdownKelamin }}</td>
+            </tr>
+        </table>
     </div>
 </template>
 <script>
     export default {
         data() {
             return {
-                bilangan1: 3,
-                bilangan2: 5,
-                total: 0,
-                keterangan: '',
-                keteranganAksi: '',
-                isi: '',
-                keteranganLogin: 'Keterangan',
-                password: '',
-                passwordRepeat: '',
+                pesanTextBox: '',
+                pesanTextArea: '',
+                pilihanRadio: '',
+                hobi: [],
+                dropdownKelamin: ''
             }
-        },
-        // untuk menangani jika ada event yang dilakukan
-        methods: {
-            proses() {
-                return this.total = this.bilangan2 + this.bilangan1;
-            },
-            mengetik() {
-                if (this.isi !== '') {
-                    this.keteranganAksi = 'Sedang mengetik... ';
-                } else {
-                    this.keteranganAksi = '';
-                }
-            },
-
-            cekPassword() {
-                if (this.password === this.passwordRepeat){
-                    this.keteranganLogin = 'Password sama';
-                }else{
-                    this.keteranganLogin = 'Password yang anda masukan berbeda';
-                }
-            }
-        },
-        // mengolah value yang berasal dari data atau dari v-model, menyimpan value
-        // perhitungan tersebut pada cache mengembalikan nilainya dan mengevaluasi perubahaannya
-        computed: {
-            jumlah: function () {
-                return this.bilangan1 + this.bilangan2;
-            }
-        },
-        // melihat perubahaan yang terjadi saat suatu aksi dilakukan
-        watch: {
-            // watch data
-            total: function () {
-                if (this.total % 2 == 0) {
-                    this.keterangan = 'Genap';
-                } else {
-                    this.keterangan = 'Ganjil';
-                }
-            },
-
-            //watch memanggil method
-            isi: 'mengetik',
-
-            passwordRepeat: 'cekPassword'
         }
     }
 </script>
