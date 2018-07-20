e0bdb9db8506bd374a191efd5bd6f93ed7a381ac
Unknown <dwirandyherdinanto@gmail.com>
2018-07-20 10:03:43 +0700

    gitflow-feature-stash: BAB 4

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import BabSatu from './views/BabSatu.vue'
 import BabDua from './views/BabDua.vue'
 import BabTiga from './views/BabTiga.vue'
+import BabEmpat from './views/BabEmpat'
 
 Vue.use(Router)
 
@@ -22,6 +23,11 @@ export default new Router({
             path: '/bab3',
             name: 'babTiga',
             component: BabTiga
+        },
+        {
+            path: '/bab4',
+            name: 'babEmpat',
+            component: BabEmpat
         }
     ]
 })
